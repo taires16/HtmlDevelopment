1c8fd6656b7ea54f874716b991304275a6570dc0
Taires <trta0610@gmail.com>
2018-10-14 22:44:19 +0700

    commit 100

--- js/profile.js
+++ js/profile.js
@@ -1,9 +1,24 @@
-var element = document.getElementById("my-img");
-
-element.addEventListener("click", alert1)
- 
-element.
-
-function alert1(){
-    alert("Clicked");
+function soNguyenTo(n){
+    if(n<2){
+        return false;
+    }
+    else{
+        for(i=2;i<n-1;i++){
+            if(n%i==0){
+                return false;
+            }
+            else{
+                return true;
+            }
+        }
+    }
 }
+var array=[3,4,1,2,5,7,8,6,9,13,11,19,14]
+var arraySnt=[]
+var snt=[]
+array.forEach(item =>{
+    arraySnt.push(item,soNguyenTo(item))
+    if(soNguyenTo(item)){
+        snt.push(item)
+    }
+})
